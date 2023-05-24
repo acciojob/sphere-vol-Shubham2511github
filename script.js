eba3f042ea5181ee35b4f61fddf91c6083483958
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-05-24 12:34:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
  
 function volume_sphere() {
     //Write your code here
-	var radius = document.getElementById('radius');
+	var radius = document.getElementById("radius");
 	let value = radius.value;
 	var volumeis = (4/3)*Math.PI*Math.pow(value,3);
-	var Volume = document.getElementById('volume');
+	var Volume = document.getElementById("volume");
 	Volume.value = volumeis
 	
   
